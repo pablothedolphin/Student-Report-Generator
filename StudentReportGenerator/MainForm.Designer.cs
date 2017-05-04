0ca233603cfe70fefa1a7c9e47a834963810c5e3
Jak <jakir.hussain@hotmail.co.uk>
2017-05-04 23:36:56 +0100

    Code added.

---
+++ StudentReportGenerator/MainForm.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace StudentReportGenerator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label genderLabel;
+            System.Windows.Forms.Label studentNameLabel;
+            System.Windows.Forms.Label currentGradeLabel;
+            System.Windows.Forms.Button addReport;
+            System.Windows.Forms.Label engagementLabel;
+            System.Windows.Forms.Label behaviour1Label;
+            System.Windows.Forms.Button saveButton;
+            System.Windows.Forms.Label behaviourLabel2;
+            this.male = new System.Windows.Forms.RadioButton();
+            this.female = new System.Windows.Forms.RadioButton();
+            this.studentNameField = new System.Windows.Forms.TextBox();
+            this.reportPreviewLabel = new System.Windows.Forms.Label();
+            this.currentGrade = new System.Windows.Forms.ComboBox();
+            this.targetGrade = new System.Windows.Forms.ComboBox();
+            this.targetGradeLabel = new System.Windows.Forms.Label();
+            this.currentReport = new System.Windows.Forms.RichTextBox();
+            this.engagement = new System.Windows.Forms.ComboBox();
+            this.behaviour1 = new System.Windows.Forms.ComboBox();
+            this.completedReportsLable = new System.Windows.Forms.Label();
+            this.behaviour2 = new System.Windows.Forms.ComboBox();
+            genderLabel = new System.Windows.Forms.Label();
+            studentNameLabel = new System.Windows.Forms.Label();
+            currentGradeLabel = new System.Windows.Forms.Label();
+            addReport = new System.Windows.Forms.Button();
+            engagementLabel = new System.Windows.Forms.Label();
+            behaviour1Label = new System.Windows.Forms.Label();
+            saveButton = new System.Windows.Forms.Button();
+            behaviourLabel2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // genderLabel
+            // 
+            genderLabel.AutoSize = true;
+            genderLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            genderLabel.Location = new System.Drawing.Point(212, 27);
+            genderLabel.Name = "genderLabel";
+            genderLabel.Size = new System.Drawing.Size(48, 13);
+            genderLabel.TabIndex = 2;
+            genderLabel.Text = "Gender: ";
+            // 
+            // studentNameLabel
+            // 
+            studentNameLabel.AutoSize = true;
+            studentNameLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            studentNameLabel.Location = new System.Drawing.Point(38, 27);
+            studentNameLabel.Name = "studentNameLabel";
+            studentNameLabel.Size = new System.Drawing.Size(41, 13);
+            studentNameLabel.TabIndex = 4;
+            studentNameLabel.Text = "Name: ";
+            // 
+            // currentGradeLabel
+            // 
+            currentGradeLabel.AutoSize = true;
+            currentGradeLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            currentGradeLabel.Location = new System.Drawing.Point(399, 27);
+            currentGradeLabel.Name = "currentGradeLabel";
+            currentGradeLabel.Size = new System.Drawing.Size(79, 13);
+            currentGradeLabel.TabIndex = 8;
+            currentGradeLabel.Text = "Current Grade: ";
+            // 
+            // addReport
+            // 
+            addReport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            addReport.Location = new System.Drawing.Point(374, 184);
+            addReport.Name = "addReport";
+            addReport.Size = new System.Drawing.Size(104, 23);
+            addReport.TabIndex = 10;
+            addReport.Text = "Add Report";
+            addReport.UseVisualStyleBackColor = true;
+            addReport.Click += new System.EventHandler(this.AddReportToList);
+            // 
+            // engagementLabel
+            // 
+            engagementLabel.AutoSize = true;
+            engagementLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            engagementLabel.Location = new System.Drawing.Point(38, 68);
+            engagementLabel.Name = "engagementLabel";
+            engagementLabel.Size = new System.Drawing.Size(73, 13);
+            engagementLabel.TabIndex = 13;
+            engagementLabel.Text = "Engagement: ";
+            // 
+            // behaviour1Label
+            // 
+            behaviour1Label.AutoSize = true;
+            behaviour1Label.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            behaviour1Label.Location = new System.Drawing.Point(230, 68);
+            behaviour1Label.Name = "behaviour1Label";
+            behaviour1Label.Size = new System.Drawing.Size(70, 13);
+            behaviour1Label.TabIndex = 14;
+            behaviour1Label.Text = "Behaviour 1: ";
+            // 
+            // saveButton
+            // 
+            saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            saveButton.Location = new System.Drawing.Point(637, 437);
+            saveButton.Name = "saveButton";
+            saveButton.Size = new System.Drawing.Size(75, 23);
+            saveButton.TabIndex = 103;
+            saveButton.Text = "Save As";
+            saveButton.UseVisualStyleBackColor = true;
+            saveButton.Click += new System.EventHandler(this.SaveReportsToFile);
+            // 
+            // male
+            // 
+            this.male.AutoSize = true;
+            this.male.Checked = true;
+            this.male.Location = new System.Drawing.Point(331, 25);
+            this.male.Name = "male";
+            this.male.Size = new System.Drawing.Size(48, 17);
+            this.male.TabIndex = 1;
+            this.male.TabStop = true;
+            this.male.Text = "Male";
+            this.male.UseVisualStyleBackColor = true;
+            this.male.CheckedChanged += new System.EventHandler(this.SetGenderMale);
+            this.male.Click += new System.EventHandler(this.SetGenderMale);
+            // 
+            // female
+            // 
+            this.female.AutoSize = true;
+            this.female.Location = new System.Drawing.Point(266, 25);
+            this.female.Name = "female";
+            this.female.Size = new System.Drawing.Size(59, 17);
+            this.female.TabIndex = 1;
+            this.female.Text = "Female";
+            this.female.UseVisualStyleBackColor = true;
+            this.female.CheckedChanged += new System.EventHandler(this.SetGenderFemale);
+            this.female.Click += new System.EventHandler(this.SetGenderFemale);
+            // 
+            // studentNameField
+            // 
+            this.studentNameField.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.studentNameField.Location = new System.Drawing.Point(85, 27);
+            this.studentNameField.Name = "studentNameField";
+            this.studentNameField.Size = new System.Drawing.Size(100, 13);
+            this.studentNameField.TabIndex = 0;
+            this.studentNameField.TextChanged += new System.EventHandler(this.SetStudentName);
+            // 
+            // reportPreviewLabel
+            // 
+            this.reportPreviewLabel.AutoSize = true;
+            this.reportPreviewLabel.Location = new System.Drawing.Point(38, 217);
+            this.reportPreviewLabel.Name = "reportPreviewLabel";
+            this.reportPreviewLabel.Size = new System.Drawing.Size(86, 13);
+            this.reportPreviewLabel.TabIndex = 100;
+            this.reportPreviewLabel.Text = "Preview Report: ";
+            // 
+            // currentGrade
+            // 
+            this.currentGrade.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.currentGrade.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.currentGrade.FormattingEnabled = true;
+            this.currentGrade.Items.AddRange(new object[] {
+            "4C",
+            "4B",
+            "4A",
+            "5C",
+            "5B",
+            "5A",
+            "6C",
+            "6B",
+            "6A",
+            "7C",
+            "7B",
+            "7A",
+            "8C",
+            "8B",
+            "8A",
+            "9C",
+            "9B",
+            "9A"});
+            this.currentGrade.Location = new System.Drawing.Point(484, 24);
+            this.currentGrade.Name = "currentGrade";
+            this.currentGrade.Size = new System.Drawing.Size(62, 21);
+            this.currentGrade.TabIndex = 2;
+            this.currentGrade.SelectedIndexChanged += new System.EventHandler(this.SetCurrentGrade);
+            this.currentGrade.Click += new System.EventHandler(this.SetCurrentGrade);
+            // 
+            // targetGrade
+            // 
+            this.targetGrade.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.targetGrade.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.targetGrade.FormattingEnabled = true;
+            this.targetGrade.Items.AddRange(new object[] {
+            "4C",
+            "4B",
+            "4A",
+            "5C",
+            "5B",
+            "5A",
+            "6C",
+            "6B",
+            "6A",
+            "7C",
+            "7B",
+            "7A",
+            "8C",
+            "8B",
+            "8A",
+            "9C",
+            "9B",
+            "9A"});
+            this.targetGrade.Location = new System.Drawing.Point(650, 24);
+            this.targetGrade.Name = "targetGrade";
+            this.targetGrade.Size = new System.Drawing.Size(62, 21);
+            this.targetGrade.TabIndex = 3;
+            this.targetGrade.SelectedIndexChanged += new System.EventHandler(this.SetTargetGrade);
+            this.targetGrade.Click += new System.EventHandler(this.SetTargetGrade);
+            // 
+            // targetGradeLabel
+            // 
+            this.targetGradeLabel.AutoSize = true;
+            this.targetGradeLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.targetGradeLabel.Location = new System.Drawing.Point(568, 27);
+            this.targetGradeLabel.Name = "targetGradeLabel";
+            this.targetGradeLabel.Size = new System.Drawing.Size(76, 13);
+            this.targetGradeLabel.TabIndex = 9;
+            this.targetGradeLabel.Text = "Target Grade: ";
+            // 
+            // currentReport
+            // 
+            this.currentReport.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.currentReport.Location = new System.Drawing.Point(41, 233);
+            this.currentReport.Name = "currentReport";
+            this.currentReport.Size = new System.Drawing.Size(671, 198);
+            this.currentReport.TabIndex = 11;
+            this.currentReport.Text = "";
+            // 
+            // engagement
+            // 
+            this.engagement.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.engagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.engagement.FormattingEnabled = true;
+            this.engagement.Items.AddRange(new object[] {
+            "disengaged",
+            "distracted",
+            "energetic",
+            "very quiet",
+            "quiet",
+            "very engaged"});
+            this.engagement.Location = new System.Drawing.Point(117, 65);
+            this.engagement.Name = "engagement";
+            this.engagement.Size = new System.Drawing.Size(92, 21);
+            this.engagement.TabIndex = 4;
+            this.engagement.SelectedIndexChanged += new System.EventHandler(this.SetEngagement);
+            // 
+            // behaviour1
+            // 
+            this.behaviour1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.behaviour1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.behaviour1.FormattingEnabled = true;
+            this.behaviour1.Items.AddRange(new object[] {
+            "rude and poorly behaved",
+            "rude but sometimes well behaved",
+            "polite but sometimes poorly behaved",
+            "polite and well behaved"});
+            this.behaviour1.Location = new System.Drawing.Point(306, 65);
+            this.behaviour1.Name = "behaviour1";
+            this.behaviour1.Size = new System.Drawing.Size(191, 21);
+            this.behaviour1.TabIndex = 5;
+            this.behaviour1.SelectedIndexChanged += new System.EventHandler(this.SetBehaviour1);
+            // 
+            // completedReportsLable
+            // 
+            this.completedReportsLable.AutoSize = true;
+            this.completedReportsLable.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.completedReportsLable.Location = new System.Drawing.Point(230, 189);
+            this.completedReportsLable.Name = "completedReportsLable";
+            this.completedReportsLable.Size = new System.Drawing.Size(109, 13);
+            this.completedReportsLable.TabIndex = 101;
+            this.completedReportsLable.Text = "Completed Reports: 0";
+            // 
+            // behaviourLabel2
+            // 
+            behaviourLabel2.AutoSize = true;
+            behaviourLabel2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            behaviourLabel2.Location = new System.Drawing.Point(522, 68);
+            behaviourLabel2.Name = "behaviourLabel2";
+            behaviourLabel2.Size = new System.Drawing.Size(70, 13);
+            behaviourLabel2.TabIndex = 104;
+            behaviourLabel2.Text = "Behaviour 2: ";
+            // 
+            // behaviour2
+            // 
+            this.behaviour2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.behaviour2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.behaviour2.FormattingEnabled = true;
+            this.behaviour2.Location = new System.Drawing.Point(598, 65);
+            this.behaviour2.Name = "behaviour2";
+            this.behaviour2.Size = new System.Drawing.Size(114, 21);
+            this.behaviour2.TabIndex = 105;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 479);
+            this.Controls.Add(this.behaviour2);
+            this.Controls.Add(behaviourLabel2);
+            this.Controls.Add(saveButton);
+            this.Controls.Add(this.completedReportsLable);
+            this.Controls.Add(this.behaviour1);
+            this.Controls.Add(behaviour1Label);
+            this.Controls.Add(engagementLabel);
+            this.Controls.Add(this.engagement);
+            this.Controls.Add(this.currentReport);
+            this.Controls.Add(addReport);
+            this.Controls.Add(this.targetGradeLabel);
+            this.Controls.Add(currentGradeLabel);
+            this.Controls.Add(this.targetGrade);
+            this.Controls.Add(this.currentGrade);
+            this.Controls.Add(this.reportPreviewLabel);
+            this.Controls.Add(studentNameLabel);
+            this.Controls.Add(this.studentNameField);
+            this.Controls.Add(genderLabel);
+            this.Controls.Add(this.female);
+            this.Controls.Add(this.male);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "Student Report Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton male;
+        private System.Windows.Forms.RadioButton female;
+        private System.Windows.Forms.TextBox studentNameField;
+        private System.Windows.Forms.Label reportPreviewLabel;
+        private System.Windows.Forms.ComboBox currentGrade;
+        private System.Windows.Forms.ComboBox targetGrade;
+        private System.Windows.Forms.Label targetGradeLabel;
+        private System.Windows.Forms.RichTextBox currentReport;
+        private System.Windows.Forms.ComboBox engagement;
+        private System.Windows.Forms.ComboBox behaviour1;
+        private System.Windows.Forms.Label completedReportsLable;
+        private System.Windows.Forms.ComboBox behaviour2;
+    }
+}
+
---
+++ StudentReportGenerator/MainForm.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace StudentReportGenerator
+{
+    public enum Grade
+    {
+        Level4C, Level4B, Level4A, Level5C, Level5B, Level5A, Level6C, Level6B, Level6A, Level7C, Level7B, Level7A, Level8C, Level8B, Level8A, Level9C, Level9B, Level9A
+    };
+
+    public partial class MainForm : Form
+    {
+        string studentName = "Example";
+        Grade studentCurrentGrade = Grade.Level4C;
+        Grade studentTargetGrade = Grade.Level4C;
+        string currentGradeText = "4C";
+        string targetGradeText = "4C";
+
+        string capitalHeShe = "He";
+        string lowerHeShe = "he";
+        string capitalHisHer = "His";
+        string lowerHisHer = "his";
+        string lowerHimHer = "him";
+
+        string studentEngagement = "disengaged";
+        string studentBehaviour1 = "rude and poorly behaved";
+        string studenBehaviour2;
+
+        int reportsWritten = 0;
+
+        List<string> students = new List<string>();
+        List<string> reports = new List<string>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            currentGrade.SelectedIndex = 0;
+            targetGrade.SelectedIndex = 0;
+            engagement.SelectedIndex = 0;
+            behaviour1.SelectedIndex = 0;
+        }
+
+        private void SetStudentName(object sender, EventArgs e)
+        {
+            studentName = studentNameField.Text;
+            UpdateReport();
+        }
+
+        private void SetGenderMale(object sender, EventArgs e)
+        {
+            capitalHeShe = "He";
+            lowerHeShe = "he";
+            capitalHisHer = "His";
+            lowerHisHer = "his";
+            lowerHimHer = "him";
+
+            UpdateReport();
+        }
+
+        private void SetGenderFemale(object sender, EventArgs e)
+        {
+            capitalHeShe = "She";
+            lowerHeShe = "she";
+            capitalHisHer = "Her";
+            lowerHisHer = "her";
+            lowerHimHer = "her";
+
+            UpdateReport();
+        }
+
+        private void SetCurrentGrade(object sender, EventArgs e)
+        {
+            currentGradeText = currentGrade.SelectedItem.ToString();
+
+            studentCurrentGrade = Grade.Level4C;
+
+            if (currentGradeText == "4B") studentCurrentGrade += 1;
+            else if (currentGradeText == "4A") studentCurrentGrade += 2;
+            else if (currentGradeText == "5C") studentCurrentGrade += 3;
+            else if (currentGradeText == "5B") studentCurrentGrade += 4;
+            else if (currentGradeText == "5A") studentCurrentGrade += 5;
+            else if (currentGradeText == "6C") studentCurrentGrade += 6;
+            else if (currentGradeText == "6B") studentCurrentGrade += 7;
+            else if (currentGradeText == "6A") studentCurrentGrade += 8;
+            else if (currentGradeText == "7C") studentCurrentGrade += 9;
+            else if (currentGradeText == "7B") studentCurrentGrade += 10;
+            else if (currentGradeText == "7A") studentCurrentGrade += 11;
+            else if (currentGradeText == "8C") studentCurrentGrade += 12;
+            else if (currentGradeText == "8B") studentCurrentGrade += 13;
+            else if (currentGradeText == "8A") studentCurrentGrade += 14;
+            else if (currentGradeText == "9C") studentCurrentGrade += 15;
+            else if (currentGradeText == "9B") studentCurrentGrade += 16;
+            else if (currentGradeText == "9A") studentCurrentGrade += 17;
+
+            UpdateReport();
+        }
+
+        private void SetTargetGrade(object sender, EventArgs e)
+        {
+            targetGradeText = targetGrade.SelectedItem.ToString();
+
+            studentTargetGrade = Grade.Level4C;
+
+            if (targetGradeText == "4B") studentTargetGrade += 1;
+            else if (targetGradeText == "4A") studentTargetGrade += 2;
+            else if (targetGradeText == "5C") studentTargetGrade += 3;
+            else if (targetGradeText == "5B") studentTargetGrade += 4;
+            else if (targetGradeText == "5A") studentTargetGrade += 5;
+            else if (targetGradeText == "6C") studentTargetGrade += 6;
+            else if (targetGradeText == "6B") studentTargetGrade += 7;
+            else if (targetGradeText == "6A") studentTargetGrade += 8;
+            else if (targetGradeText == "7C") studentTargetGrade += 9;
+            else if (targetGradeText == "7B") studentTargetGrade += 10;
+            else if (targetGradeText == "7A") studentTargetGrade += 11;
+            else if (targetGradeText == "8C") studentTargetGrade += 12;
+            else if (targetGradeText == "8B") studentTargetGrade += 13;
+            else if (targetGradeText == "8A") studentTargetGrade += 14;
+            else if (targetGradeText == "9C") studentTargetGrade += 15;
+            else if (targetGradeText == "9B") studentTargetGrade += 16;
+            else if (targetGradeText == "9A") studentTargetGrade += 17;
+
+            UpdateReport();
+        }
+
+        private void SetEngagement(object sender, EventArgs e)
+        {
+            studentEngagement = engagement.SelectedItem.ToString();
+
+            UpdateReport();
+        }
+
+        private void SetBehaviour1(object sender, EventArgs e)
+        {
+            studentBehaviour1 = behaviour1.SelectedItem.ToString();
+
+            UpdateReport();
+        }
+
+        private void UpdateReport()
+        {
+            currentReport.Text = "" + studentName + " is a " + studentEngagement + " member of " + lowerHisHer + " class. ";
+
+            currentReport.Text += capitalHeShe + " is " + studentBehaviour1 + ", ";
+
+            currentReport.Text += "\n\n" + studentName + " is currently working at a level " + currentGradeText + " which is ";
+
+            if (studentCurrentGrade < studentTargetGrade) currentReport.Text += "bellow ";
+            else if (studentCurrentGrade == studentTargetGrade) currentReport.Text += "on ";
+            else currentReport.Text += "above ";
+
+            currentReport.Text += lowerHisHer + " target for the year. ";
+
+            if (studentCurrentGrade < studentTargetGrade)
+                currentReport.Text += "However, with some extremely hard work " + lowerHeShe + " has the potential to achieve " + lowerHisHer + " target of " + targetGradeText + ". ";
+
+            else if (studentCurrentGrade == studentTargetGrade)
+                currentReport.Text += "If " + lowerHeShe + " stays consistent in " + lowerHisHer + " performance, there is no doubt " + lowerHeShe + " can aim higher than a " + targetGradeText + ". ";
+
+            else
+                currentReport.Text += "With some more attention to " + lowerHisHer + " classwork, " + lowerHeShe + " can easily begin to accel in Computing. ";
+
+            currentReport.Text += "\n\n";
+        }
+
+        private void AddReportToList(object sender, EventArgs e)
+        {
+            reportsWritten++;
+            completedReportsLable.Text = "Completed Reports: " + reportsWritten;
+
+            students.Add(studentName);
+            reports.Add(currentReport.Text);
+        }
+
+        private void SaveReportsToFile(object sender, EventArgs e)
+        {
+            SaveFileDialog mySaveFileDialog = new SaveFileDialog();
+            mySaveFileDialog.Filter = "txt files (*.txt)|*.txt";
+            mySaveFileDialog.RestoreDirectory = true;
+
+            if (mySaveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (Stream myStream = File.Open(mySaveFileDialog.FileName, FileMode.Create))
+                using (StreamWriter myStreamWriter = new StreamWriter(myStream))
+                {
+                    for (int i = 0; i < reports.Count; i++)
+                    {
+                        myStreamWriter.WriteLine(students[i] + "'s Report:");
+                        myStreamWriter.WriteLine("");
+
+                        string[] reportLine = reports[i].Split('\n');
+                        for (int j = 0; j < reportLine.Length; j++)
+                        {
+                            myStreamWriter.WriteLine(reportLine[j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
